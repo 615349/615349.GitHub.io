9588fc2d792dbc66628c5614545dfb8e1da9f57d
Ben Li <huanli198@gmail.com>
2023-06-26 20:30:18 +1000

    add name

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,7 +5,6 @@ const Wrapper = styled.div`
   background-color: #f5f5f5;
   width: 100vw;
   height: 100vh;
-  display: flex;
 `;
 
 const Inner = styled.div`
@@ -15,12 +14,22 @@ const Inner = styled.div`
   margin: 30px auto;
 `;
 
+const Name = styled.div`
+  display: flex;
+  font-size: 24px;
+  letter-spacing: -1px;
+  padding: 30px;
+  text-decoration: none;
+  color: hsl(245deg, 100%, 60%);
+`;
+
 const Home = () => {
   return (
     <Wrapper>
+      <Name>Ben's code sharing</Name>
       <Inner>
         <Section href="array-flatten" text="Array flatten" />
-        <Section href='optional' text='TS - Optional' />
+        <Section href="optional" text="TS - Optional" />
       </Inner>
     </Wrapper>
   );
